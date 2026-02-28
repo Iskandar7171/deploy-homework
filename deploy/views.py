80de4953fb802d708f2273cd3a6605a2f7be6e0d
iskandar7171 <tolagankhojaeviskandar@gmail.com>
2026-02-28 23:06:59 +0300

    qwe

---
+++ deploy/views.py
@@ -0,0 +1,21 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Student
+from rest_framework.permissions import AllowAny
+from rest_framework.pagination import PageNumberPagination
+from .serializers import StudentSerializer
+
+class CustomPaginator(PageNumberPagination):
+    page_size = 20
+
+
+class StudentListCreateApiVIew(ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [AllowAny]
+    pagination_class = CustomPaginator
+
+
+class StudentDetailApiView(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    permission_classes = [AllowAny]
